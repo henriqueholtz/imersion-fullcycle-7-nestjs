557b94aac31bfd606df26480e7c1dbbcdbefbfb7
henriqueholtz <henrique_holtz@hotmail.com>
2022-04-02 11:19:02 -0300

    Configure Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Warm up! Imersion 7.0 API Rest (by: FullCycle)')
+    .setDescription('API REST to manipulate the orders')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
