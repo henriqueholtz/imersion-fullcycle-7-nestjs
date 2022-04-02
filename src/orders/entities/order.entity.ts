a010af98907e1ec3ec86e4666be3aef270327bd5
henriqueholtz <henrique_holtz@hotmail.com>
2022-04-02 10:32:23 -0300

    Added imports modules (to Order Model) SQLite configurations and create Orders table

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,1 +1,17 @@
-export class Order {}
+import {
+  Column,
+  DataType,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
+
+@Table({ tableName: 'Orders' })
+export class Order extends Model {
+  @PrimaryKey
+  @Column({ type: DataType.UUID, defaultValue: DataType.UUIDV4 })
+  OrderId: string;
+
+  @Column({ allowNull: false, type: DataType.DECIMAL(10, 2) })
+  Amount: number;
+}
